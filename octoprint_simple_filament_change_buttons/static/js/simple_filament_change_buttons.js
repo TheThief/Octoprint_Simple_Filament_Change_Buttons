1d565d1856847aa8a287386cd62f3841d8587352
StevilKnevil <stevilknevil@hotmail.co.uk>
2023-04-05 08:01:05 +0100

    Add a Temperature control (#3)
    Will heat the nozzle to at least the set temp for load/unload/change, and restore prior temp afterwards. This is mostly so the buttons work when the printer is cold, rather than just putting a "too cold" error in the log.

--- octoprint_simple_filament_change_buttons/static/js/simple_filament_change_buttons.js
+++ octoprint_simple_filament_change_buttons/static/js/simple_filament_change_buttons.js
@@ -11,6 +11,68 @@ $(function() {
 		// assign the injected parameters
 		self.controlViewModel = parameters[0];
 
+		self.unloadFilament = function() {
+			self.currentTargetTemperature().then(temp => {
+				cmds = [
+					// Heat to specified temp
+					"M109 S" + self.desiredTemperature(),
+					// Do unload
+					"M702",
+					// Cool hotend
+					"M104 S" + temp
+				]	
+				OctoPrint.control.sendGcode(cmds)
+			})
+		}
+
+		self.loadFilament = function() {
+			self.currentTargetTemperature().then(temp => {
+				cmds = [
+					// Heat to specified temp
+					"M109 S" + self.desiredTemperature(),
+					// Do load
+					"M701",
+					// Cool hotend
+					"M104 S" + temp
+				]	
+				OctoPrint.control.sendGcode(cmds)
+			})
+		}
+
+		self.changeFilament = function() {
+			self.currentTargetTemperature().then(temp => {
+				cmds = [
+					// Heat to specified temp
+					"M109 S" + self.desiredTemperature(),
+					// Do filament change
+					"M600",
+					// Cool hotend
+					"M104 S" + temp
+				]	
+				OctoPrint.control.sendGcode(cmds)
+			})        
+		}
+
+		self.resume = function() {
+			cmds = [
+				'M108'
+			]		
+			OctoPrint.control.sendGcode(cmds)
+		}
+
+		self.desiredTemperature = function()
+		{
+			// The temperature input field has a "placeholder" attribute that is equal to it's name
+			return $('*[placeholder = "Temperature"]')[0].value
+		}
+
+		self.currentTargetTemperature = async function()
+		{
+			state = await OctoPrint.printer.getToolState();
+			return state.tool0.target
+		}		
+
+
 		self.getAdditionalControls = function() {
 			return [{
 				'layout': 'horizontal', 'name': 'Filament Change', 'children':[
@@ -18,22 +80,17 @@ $(function() {
 						'M125'
 						],
 						'additionalClasses': 'sfcb fa-pause', 'name': ' Park'},*/
-					{'commands': [
-						'M702'
-						],
-						'additionalClasses': 'sfcb fa-fast-backward', 'name': ' Unload'},
-					{'commands': [
-						'M701'
-						],
-						'additionalClasses': 'sfcb fa-step-forward', 'name': ' Load'},
-					{'commands': [
-						'M600'
-						],
-						'additionalClasses': 'sfcb fa-sync', 'name': ' Change Filament'},
-					{'commands': [
-						'M108'
-						],
-						'additionalClasses': 'sfcb fa-play', 'name': ' Resume'},
+
+					{name: ' Unload', javascript: self.unloadFilament, additionalClasses: 'sfcb fa-fast-backward'},
+					{name: ' Load', javascript: self.loadFilament, additionalClasses: 'sfcb fa-step-forward'},
+					{name: ' Change Filament', javascript: self.changeFilament, additionalClasses: 'sfcb fa-sync'},
+					{name: ' Resume', javascript: self.resume, additionalClasses: 'sfcb fa-play'},
+
+					{'input': [{
+							name: 'Temperature',
+							default: 200
+						}]
+					},
 				]
 			}];
 		};
